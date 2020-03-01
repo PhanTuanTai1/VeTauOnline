b470d770b819c74ef62204c25d6b99da0f0be135
Phan Tuan Tai <phantuantai1234@gmail.com>
2020-03-01 18:39:41 +0700

    save

--- controller/indexController.js
+++ controller/indexController.js
@@ -1,12 +1,13 @@
 var { Sequelize, Model, DataTypes } = require('sequelize');
 var db = require("../data_access/DataAccess");
-var encode = require("punycode");
+
+
 
 module.exports.index = function(req,res) {
     db.Station.findAll({
         attributes:["ID","Name"],
         tableName : "Station"
-    }).then(station =>{       
-        res.end();
+    }).then(station =>{           
+        res.end(JSON.jsFriendlyJSONStringify(station));
     })
 }
