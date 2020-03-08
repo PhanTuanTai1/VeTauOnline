3a6bf67075bc7757bb6725471e3e169d46ff0692
phantuantai1234@gmail.com <phantuantai1234@gmail.com>
2020-03-08 18:17:21 +0700

    savE

--- UI/dist/myVue.js
+++ UI/dist/myVue.js
@@ -1,10 +1,25 @@
 new Vue({
-    el: '#app',
-    vuetify: new Vuetify(),
-    data: () => ({ 
-    departDate: new Date().toISOString().substr(0, 10),
-    returnDate: new Date().toISOString().substr(0, 10),
-    menu2: false,
-    menu: false,
-  }),
+  el:'#app_to',
+  vuetify: new Vuetify(),
+  
 })
+
+new Vue({
+  el: "#app_from",
+  vuetify: new Vuetify(),
+ 
+ 
+})
+
+new Vue({
+  el: '#app',
+  vuetify: new Vuetify(),
+  data: () => ({ 
+  departDate: new Date().toISOString().substr(0, 10),
+  returnDate: new Date().toISOString().substr(0, 10),
+  menu2: false,
+  menu: false,
+  
+}),
+})
+
