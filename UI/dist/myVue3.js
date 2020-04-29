ca640f5dfb782ceaa4b5593d76d6b68a5e509371
phantuantai1234@gmail.com <phantuantai1234@gmail.com>
2020-04-29 17:42:24 +0700

    save

--- UI/dist/myVue3.js
+++ UI/dist/myVue3.js
@@ -119,6 +119,7 @@ var vm = new Vue({
             alert(JSON.stringify(this.result))
             alert(JSON.stringify(this.listCarriage))
             alert(JSON.stringify(this.listCarriage[0].Seats[0].SeatTypeID))
+            alert(JSON.stringify(this.result.ScheduleDetails[0].TableCosts[0].SeatTypeID));
             var result = this.listCarriage.filter(data => {
                 return data.TrainID == this.result.TrainID && data.Seats[0].SeatTypeID == this.result.ScheduleDetails[0].TableCosts[0].SeatTypeID
             })
