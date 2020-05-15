b0d1f6c7a12dd5c7fd8ab710298b195809f5cd3a
lamhuy0302 <hacklachek113@gmail.com>
2020-05-15 16:51:17 +0700

    save

--- data_access/DataAccess.js
+++ data_access/DataAccess.js
@@ -15,7 +15,7 @@ Sequelize.DATE.prototype._stringify = function _stringify(date, options) {
 // })
 
 // database local
-var sequelize = new Sequelize('TrainTicketDatabase', 'sa', '79495291', {
+var sequelize = new Sequelize('TrainTicketDatabase', 'sa', '123456789', {
     dialect: 'mssql',
     host: 'localhost',
     port: '1433',
@@ -31,198 +31,201 @@ var sequelize = new Sequelize('TrainTicketDatabase', 'sa', '79495291', {
 // })
 
 //1 Train Entity
-class Train extends Model{}
+class Train extends Model { }
 Train.init({
-    'ID' : {type: Sequelize.INTEGER,primaryKey: true},
-    'Name' : Sequelize.STRING
+    'ID': { type: Sequelize.INTEGER, primaryKey: true },
+    'Name': Sequelize.STRING
 },
-{ 
-    sequelize, 
-    modelName: 'Train',
-    tableName: 'Train',
-    timestamps: false,
-});
+    {
+        sequelize,
+        modelName: 'Train',
+        tableName: 'Train',
+        timestamps: false,
+    });
 
 //2 Carriage Entity
-class Carriage extends Model{}
+class Carriage extends Model { }
 Carriage.init({
-    'ID' : {type: Sequelize.INTEGER,primaryKey: true},
-    'Name' : Sequelize.STRING
+    'ID': { type: Sequelize.INTEGER, primaryKey: true },
+    'Name': Sequelize.STRING
 },
-{ 
-    sequelize, 
-    modelName: 'Carriage',
-    tableName: 'Carriage',
-    timestamps: false,
-});
+    {
+        sequelize,
+        modelName: 'Carriage',
+        tableName: 'Carriage',
+        timestamps: false,
+    });
 
 //3 Seat Entity
-class Seat extends Model{}
+class Seat extends Model { }
 Seat.init({
-    'ID' : {type: Sequelize.INTEGER,primaryKey: true},
-    'SeatNumber' : Sequelize.INTEGER
+    'ID': { type: Sequelize.INTEGER, primaryKey: true },
+    'SeatNumber': Sequelize.INTEGER,
+    'SeatTypeID': Sequelize.INTEGER,
+    'CarriageID': Sequelize.INTEGER,
 },
-{ 
-    sequelize, 
-    modelName: 'Seat',
-    tableName: 'Seat',
-    timestamps: false,
-});
+    {
+        sequelize,
+        modelName: 'Seat',
+        tableName: 'Seat',
+        timestamps: false,
+    });
 
 //4 SeatType Entity
-class SeatType extends Model{}
+class SeatType extends Model { }
 SeatType.init({
-    'ID' : {type: Sequelize.INTEGER,primaryKey: true},
-    'TypeName' : Sequelize.STRING,
+    'ID': { type: Sequelize.INTEGER, primaryKey: true },
+    'TypeName': Sequelize.STRING,
     'CostPerKm': Sequelize.INTEGER
 },
-{ 
-    sequelize, 
-    modelName: 'SeatType',
-    tableName: 'SeatType',
-    timestamps: false,
-});
+    {
+        sequelize,
+        modelName: 'SeatType',
+        tableName: 'SeatType',
+        timestamps: false,
+    });
 
 //5 Schedule Entity
-class Schedule extends Model{}
+class Schedule extends Model { }
 Schedule.init({
-    'ID' : {type: Sequelize.INTEGER,primaryKey: true},
-    'DateDeparture' : Sequelize.DATE,
-    'TimeDeparture' : Sequelize.STRING
+    'ID': { type: Sequelize.INTEGER, primaryKey: true },
+    'DateDeparture': Sequelize.DATE,
+    'TimeDeparture': Sequelize.STRING
 },
-{ 
-    sequelize, 
-    modelName: 'Schedule',
-    tableName: 'Schedule',
-    timestamps: false,
-});
+    {
+        sequelize,
+        modelName: 'Schedule',
+        tableName: 'Schedule',
+        timestamps: false,
+    });
 
 //6 ScheduleDetail Entity
-class ScheduleDetail extends Model{}
+class ScheduleDetail extends Model { }
 ScheduleDetail.init({
-    'ID' : {type: Sequelize.INTEGER,primaryKey: true},
-    'Length' : Sequelize.INTEGER,
-    'Time' : Sequelize.FLOAT,
-    'StartTime' : Sequelize.TIME
+    'ID': { type: Sequelize.INTEGER, primaryKey: true },
+    'Length': Sequelize.INTEGER,
+    'Time': Sequelize.FLOAT,
+    'StartTime': Sequelize.TIME
 },
-{ 
-    sequelize, 
-    modelName: 'ScheduleDetail',
-    tableName: 'ScheduleDetail',
-    timestamps: false,
-});
+    {
+        sequelize,
+        modelName: 'ScheduleDetail',
+        tableName: 'ScheduleDetail',
+        timestamps: false,
+    });
 
 //7 Station Entity
-class Station extends Model{}
+class Station extends Model { }
 Station.init({
-    'ID' : {type: Sequelize.INTEGER,primaryKey: true},
-    'Name' : Sequelize.STRING,
+    'ID': { type: Sequelize.INTEGER, primaryKey: true },
+    'Name': Sequelize.STRING,
     'Location': Sequelize.STRING,
     'Distance': Sequelize.INTEGER
 },
-{ 
-    sequelize, 
-    modelName: 'Station',
-    tableName: 'Station',
-    timestamps: false,
-});
+    {
+        sequelize,
+        modelName: 'Station',
+        tableName: 'Station',
+        timestamps: false,
+    });
 
 //8 Ticket Entity
-class Ticket extends Model{}
+class Ticket extends Model { }
 Ticket.init({
-    'ID' : {type: Sequelize.INTEGER,primaryKey: true},
-    'Price' : Sequelize.FLOAT,
-    'DepartureDate' : Sequelize.DATE,
-    'DepartureTime' : Sequelize.TIME,
-    'TrainName' : Sequelize.STRING,
-    'Status' : Sequelize.BOOLEAN
+    'ID': { type: Sequelize.INTEGER, primaryKey: true },
+    'Price': Sequelize.FLOAT,
+    'DepartureDate': Sequelize.DATE,
+    'DepartureTime': Sequelize.TIME,
+    'TrainName': Sequelize.STRING,
+    'Status': Sequelize.BOOLEAN
 },
-{ 
-    sequelize, 
-    modelName: 'Ticket',
-    tableName: 'Ticket',
-    timestamps: false,
-});
+    {
+        sequelize,
+        modelName: 'Ticket',
+        tableName: 'Ticket',
+        timestamps: false,
+    });
 
 //9 TableCost Entity
-class TableCost extends Model{}
+class TableCost extends Model { }
 TableCost.init({
-    'ScheduleID' : {type: Sequelize.INTEGER, primaryKey: true},
-    'SeatTypeID' : {type: Sequelize.INTEGER, primaryKey: true},
-    'Cost' : Sequelize.FLOAT,
+    'ScheduleID': { type: Sequelize.INTEGER, primaryKey: true },
+    'SeatTypeID': { type: Sequelize.INTEGER, primaryKey: true },
+    'Cost': Sequelize.FLOAT,
 },
-{ 
-    sequelize, 
-    modelName: 'TableCost',
-    tableName: 'TableCost',
-    timestamps: false,
-});
+    {
+        sequelize,
+        modelName: 'TableCost',
+        tableName: 'TableCost',
+        timestamps: false,
+    });
 
 //10 TypeObject Entity
-class TypeObject extends Model{}
+class TypeObject extends Model { }
 TypeObject.init({
-    'ID' : {type: Sequelize.INTEGER,primaryKey: true},
-    'TypeObjectName' : Sequelize.STRING,
+    'ID': { type: Sequelize.INTEGER, primaryKey: true },
+    'TypeObjectName': Sequelize.STRING,
     'Discount': Sequelize.FLOAT
 },
-{ 
-    sequelize, 
-    modelName: 'TypeObject',
-    tableName: 'TypeObject',
-    timestamps: false,
-});
+    {
+        sequelize,
+        modelName: 'TypeObject',
+        tableName: 'TypeObject',
+        timestamps: false,
+    });
 
 //11 Representative Entity
-class Representative extends Model{}
+class Representative extends Model { }
 Representative.init({
-    'ID' : {type: Sequelize.INTEGER,primaryKey: true},
-    'Email' : Sequelize.STRING,
-    'Phone' : Sequelize.STRING,
-    'Passport' : Sequelize.STRING,
-    'TotalCost' : Sequelize.FLOAT,
-    'Name' : Sequelize.STRING,
-    'DateBooking' : Sequelize.DATE
+    'ID': { type: Sequelize.INTEGER, primaryKey: true },
+    'Email': Sequelize.STRING,
+    'Phone': Sequelize.STRING,
+    'Passport': Sequelize.STRING,
+    'TotalCost': Sequelize.FLOAT,
+    'Name': Sequelize.STRING,
+    'DateBooking': Sequelize.DATE
 },
-{ 
-    sequelize, 
-    modelName: 'Representative',
-    tableName: 'Representative',
-    timestamps: false,
-});
+    {
+        sequelize,
+        modelName: 'Representative',
+        tableName: 'Representative',
+        timestamps: false,
+    });
 
 //12 Customer Entity
-class Customer extends Model{}
+class Customer extends Model { }
 Customer.init({
-    'ID' : {type: Sequelize.INTEGER,primaryKey: true},
-    'Name' : Sequelize.STRING,
-    'Passport' : Sequelize.STRING,
+    'ID': { type: Sequelize.INTEGER, primaryKey: true },
+    'Name': Sequelize.STRING,
+    'Passport': Sequelize.STRING,
 },
-{ 
-    sequelize, 
-    modelName: 'Customer',
-    tableName: 'Customer',
-    timestamps: false,
-});
+    {
+        sequelize,
+        modelName: 'Customer',
+        tableName: 'Customer',
+        timestamps: false,
+    });
 
-Train.hasMany(Carriage,{foreignKey :"TrainID"});
-Carriage.hasMany(Seat,{foreignKey :"CarriageID"});
-Seat.hasMany(Ticket,{foreignKey:"SeatID"});
-Seat.belongsTo(SeatType,{foreignKey:"SeatTypeID"});
-Schedule.belongsTo(Train,{foreignKey:"TrainID"});
-Schedule.hasMany(ScheduleDetail,{foreignKey:"ScheduleID"});
-ScheduleDetail.belongsTo(Station,{foreignKey:"DepartureStationID"});
-ScheduleDetail.belongsTo(Station,{foreignKey:"ArrivalStationID"});
+Train.hasMany(Carriage, { foreignKey: "TrainID" });
+Carriage.hasMany(Seat, { foreignKey: "CarriageID" });
+Seat.hasMany(Ticket, { foreignKey: "SeatID" });
+Seat.belongsTo(Carriage, { foreignKey: "CarriageID" });
+Seat.belongsTo(SeatType, { foreignKey: "SeatTypeID" });
+Schedule.belongsTo(Train, { foreignKey: "TrainID" });
+Schedule.hasMany(ScheduleDetail, { foreignKey: "ScheduleID" });
+ScheduleDetail.belongsTo(Station, { foreignKey: "DepartureStationID" });
+ScheduleDetail.belongsTo(Station, { foreignKey: "ArrivalStationID" });
 ScheduleDetail.belongsTo(Schedule);
-TableCost.belongsTo(SeatType,{foreignKey:"ID"});
-TableCost.belongsTo(ScheduleDetail,{foreignKey:"ID"});
-Representative.hasMany(Customer,{foreignKey:"RepresentativeID"});
-Customer.belongsTo(Representative,{foreignKey:"ID"});
-Customer.belongsTo(TypeObject,{foreignKey:"TypeObjectID"});
-Ticket.belongsTo(Customer,{foreignKey:"CustomerID"});
-Ticket.belongsTo(Seat,{foreignKey: "SeatID"});
-ScheduleDetail.hasMany(TableCost,{foreignKey:"ScheduleID"})
-Ticket.belongsTo(Station, {foreignKey: 'DepartureStationID'})
-Ticket.belongsTo(Station, {foreignKey: 'ArrivalStationID'})
+TableCost.belongsTo(SeatType, { foreignKey: "ID" });
+TableCost.belongsTo(ScheduleDetail, { foreignKey: "ID" });
+Representative.hasMany(Customer, { foreignKey: "RepresentativeID" });
+Customer.belongsTo(Representative, { foreignKey: "ID" });
+Customer.belongsTo(TypeObject, { foreignKey: "TypeObjectID" });
+Ticket.belongsTo(Customer, { foreignKey: "CustomerID" });
+Ticket.belongsTo(Seat, { foreignKey: "SeatID" });
+ScheduleDetail.hasMany(TableCost, { foreignKey: "ScheduleID" })
+Ticket.belongsTo(Station, { foreignKey: 'DepartureStationID' })
+Ticket.belongsTo(Station, { foreignKey: 'ArrivalStationID' })
 
 module.exports.Train = Train;
 module.exports.Carriage = Carriage;
